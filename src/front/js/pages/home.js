475a1e3fe7d55c2a877397c3eb685a8a1c705162
COCOASOO <85440064+COCOASOO@users.noreply.github.com>
2024-06-04 18:38:07 +0000

    nueva estructura de pantallas

--- src/front/js/pages/home.js
+++ src/front/js/pages/home.js
@@ -1,18 +1,23 @@
 import React, { useContext } from "react";
 import { Context } from "../store/appContext";
-import rigoImageUrl from "../../img/rigo-baby.jpg";
 import { Link } from "react-router-dom";
 import "../../styles/home.css";
 
 export const Home = () => {
-	const { store, actions } = useContext(Context);
+    const { store, actions } = useContext(Context);
 
-	return (
-		<div className="text-center mt-5 ">
-			<h3>example text</h3>
-			<Link to={"/clientportal"}>aaaa</Link>
-			<br></br>
-			<Link to={"/adminportal"}>bbbb</Link>
-		</div>
-	);
+    return (
+        <div className="text-center mt-5">
+            <h2>Navegaciones de prueba. El landing es lo último</h2>
+            <p>Las paginas de demo y single no borrarlas de momento por si nos sirven de alguna duda</p>
+            <ul className="list-unstyled">
+                <li className="nav-item"><Link className="nav-link" to="/clientportal">Client Portal</Link></li>
+                <li className="nav-item"><Link className="nav-link" to="/adminportal">Admin Portal</Link></li>
+                <li className="nav-item"><Link className="nav-link" to="/services">Services</Link></li>
+                <li className="nav-item"><Link className="nav-link" to="/services/companyprofile">Company Profile for User</Link></li>
+                <li className="nav-item"><Link className="nav-link" to="/login">Login</Link></li>
+                <li className="nav-item"><Link className="nav-link" to="/signin">Signin</Link></li>
+            </ul>
+        </div>
+    );
 };
---
+++ src/front/js/pages/signin.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Signin = () => {
+    return (
+        <div>
+            <h2>sigin</h2>
+            
+        </div>
+    );
+};
+
+export default Signin;
