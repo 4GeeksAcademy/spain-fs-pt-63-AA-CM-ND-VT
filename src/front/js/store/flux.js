ae6b760eb885ca531fc43183b8447f063f82e210
COCOASOO <85440064+COCOASOO@users.noreply.github.com>
2024-06-15 10:47:21 +0000

    a

--- src/front/js/store/flux.js
+++ src/front/js/store/flux.js
@@ -17,42 +17,42 @@ const getState = ({ getStore, getActions, setStore }) => {
 			// ---- apartado sesiones
 
 			login: async (email, password) => {
-                const opts = {
-                    method: 'POST',
-                    headers: {
-                        "Content-Type": "application/json"
-                    },
-                    body: JSON.stringify({
-                        "email": email,
-                        "password": password
-                    })
-                };
-                try {
-                    const resp = await fetch(`${process.env.BACKEND_URL}/api/login`, opts);
-                    if (resp.status !== 200) {
-                        alert("There has been some error");
-                        return false;
-                    }
-                    const data = await resp.json();
-                    sessionStorage.setItem("token", data.access_token);
-                    sessionStorage.setItem("username", data.username);
-                    sessionStorage.setItem("user_id", data.user_id);
-                    sessionStorage.setItem("rol", data.rol);
-                    sessionStorage.setItem("companyname", data.companyname);
-                    sessionStorage.setItem("company_id", data.company_id);
-                    setStore({
-                        token: data.access_token,
-                        username: data.username,
-                        user_id: data.user_id,
-                        rol: data.rol,
-                        companyname: data.companyname,
-                        company_id: data.company_id
-                    });
-                    return true;
-                } catch (error) {
-                    console.log(error);
-                }
-            },
+				const opts = {
+					method: 'POST',
+					headers: {
+						"Content-Type": "application/json"
+					},
+					body: JSON.stringify({
+						"email": email,
+						"password": password
+					})
+				};
+				try {
+					const resp = await fetch(`${process.env.BACKEND_URL}/api/login`, opts);
+					if (resp.status !== 200) {
+						alert("There has been some error");
+						return false;
+					}
+					const data = await resp.json();
+					sessionStorage.setItem("token", data.access_token);
+					sessionStorage.setItem("username", data.username);
+					sessionStorage.setItem("user_id", data.user_id);
+					sessionStorage.setItem("rol", data.rol);
+					sessionStorage.setItem("companyname", data.companyname);
+					sessionStorage.setItem("company_id", data.company_id);
+					setStore({
+						token: data.access_token,
+						username: data.username,
+						user_id: data.user_id,
+						rol: data.rol,
+						companyname: data.companyname,
+						company_id: data.company_id
+					});
+					return true;
+				} catch (error) {
+					console.log(error);
+				}
+			},
 
 			signup: async (email, password, name, rol) => {
 				const opts = {
@@ -119,48 +119,48 @@ const getState = ({ getStore, getActions, setStore }) => {
 			},
 
 			syncToken: () => {
-                const token = sessionStorage.getItem("token");
-                const user_id = sessionStorage.getItem("user_id");
-                const username = sessionStorage.getItem("username");
-                const rol = sessionStorage.getItem("rol");
+				const token = sessionStorage.getItem("token");
+				const user_id = sessionStorage.getItem("user_id");
+				const username = sessionStorage.getItem("username");
+				const rol = sessionStorage.getItem("rol");
 
-                if (token) {
-                    setStore({
-                        ...getStore(),
-                        token: token,
-                        user_id: user_id,
-                        username: username,
-                        rol: rol
-                    });
-                }
-            },
+				if (token) {
+					setStore({
+						...getStore(),
+						token: token,
+						user_id: user_id,
+						username: username,
+						rol: rol
+					});
+				}
+			},
 
 			logout: async () => {
-                const opts = {
-                    method: 'POST',
-                    headers: {
-                        "Content-Type": "application/json",
-                        "Authorization": "Bearer " + getStore().token
-                    }
-                };
-                try {
-                    await fetch(`${process.env.BACKEND_URL}/api/logout`, opts);
-                    sessionStorage.removeItem("token");
-                    sessionStorage.removeItem("user_id");
-                    sessionStorage.removeItem("username");
-                    sessionStorage.removeItem("rol");
-                    setStore({
-                        token: null,
-                        username: null,
-                        user_id: null,
-                        rol: null,
-                        companyname: null,
-                        company_id: null
-                    });
-                } catch (error) {
-                    console.log(error);
-                }
-            },
+				const opts = {
+					method: 'POST',
+					headers: {
+						"Content-Type": "application/json",
+						"Authorization": "Bearer " + getStore().token
+					}
+				};
+				try {
+					await fetch(`${process.env.BACKEND_URL}/api/logout`, opts);
+					sessionStorage.removeItem("token");
+					sessionStorage.removeItem("user_id");
+					sessionStorage.removeItem("username");
+					sessionStorage.removeItem("rol");
+					setStore({
+						token: null,
+						username: null,
+						user_id: null,
+						rol: null,
+						companyname: null,
+						company_id: null
+					});
+				} catch (error) {
+					console.log(error);
+				}
+			},
 
 			// ---- apartado imagenes
 
@@ -292,7 +292,7 @@ const getState = ({ getStore, getActions, setStore }) => {
 						status: "Pendiente",
 						comment: ""
 					};
-			
+
 					const optsRequest = {
 						method: "POST",
 						headers: {
@@ -301,7 +301,7 @@ const getState = ({ getStore, getActions, setStore }) => {
 						},
 						body: JSON.stringify(dataRequest),
 					};
-			
+
 					try {
 						const respRequest = await fetch(`${process.env.BACKEND_URL}/api/requests`, optsRequest);
 						if (respRequest.status !== 201) {
@@ -313,12 +313,17 @@ const getState = ({ getStore, getActions, setStore }) => {
 						console.log(error);
 						return false;
 					}
+				} catch (e) {
+					console.log(e);
+				}
+			},
+
 
 			getUser: async (user_id) => {
 				const store = getStore();
-				console.log("dentro del flux " ,user_id)
+				console.log("dentro del flux ", user_id)
 				try {
-					const resp = await fetch(`${process.env.BACKEND_URL}/api/clientportal/${user_id}`,{
+					const resp = await fetch(`${process.env.BACKEND_URL}/api/clientportal/${user_id}`, {
 						headers: {
 							"Content-Type": "application/json",
 							"Authorization": `Bearer ${store.token}`
@@ -330,17 +335,17 @@ const getState = ({ getStore, getActions, setStore }) => {
 					const data = await resp.json();
 					console.log(data)
 					setStore({ user: data });
-					return data; 
+					return data;
 				} catch (error) {
 					console.error('Error fetching company:', error);
-					throw error;  
+					throw error;
 				}
 			},
-			
+
 			updateUser: async (user_id, userData) => {
 				const store = getStore();
-				const token = store.token;  
-			
+				const token = store.token;
+
 				try {
 					const response = await fetch(`${process.env.BACKEND_URL}/api/clientportal/${user_id}`, {
 						method: 'PUT',
@@ -350,11 +355,11 @@ const getState = ({ getStore, getActions, setStore }) => {
 						},
 						body: JSON.stringify(userData)
 					});
-			
+
 					if (!response.ok) {
 						throw new Error('Failed to update user');
 					}
-			
+
 					const data = await response.json();
 					return data;
 				} catch (error) {
@@ -387,7 +392,7 @@ const getState = ({ getStore, getActions, setStore }) => {
 
 			// ---- Tema reservas user
 
-			 getUserBookings: async () => {
+			getUserBookings: async () => {
 				const userId = sessionStorage.getItem('user_id');
 				if (!userId) {
 					alert("User ID is missing. Please log in again.");
@@ -412,8 +417,8 @@ const getState = ({ getStore, getActions, setStore }) => {
 					return [];
 				}
 			},
-			
-			 getUserRequests : async () => {
+
+			getUserRequests: async () => {
 				const userId = sessionStorage.getItem('user_id');
 				if (!userId) {
 					alert("User ID is missing. Please log in again.");
@@ -438,63 +443,61 @@ const getState = ({ getStore, getActions, setStore }) => {
 					return [];
 				}
 			},
-			
-			
+
+
 		},
 
-			getCompany: async (company_id) => {
-				const store = getStore();
-				console.log("dentro del flux " ,company_id)
-				try {
-					const resp = await fetch(`${process.env.BACKEND_URL}/api/adminportal/${company_id}`,{
-						headers: {
-							"Content-Type": "application/json",
-							"Authorization": `Bearer ${store.token}`
-						}
-					});
-					if (!resp.ok) {
-						throw new Error('Failed to fetch company');
+		getCompany: async (company_id) => {
+			const store = getStore();
+			console.log("dentro del flux ", company_id)
+			try {
+				const resp = await fetch(`${process.env.BACKEND_URL}/api/adminportal/${company_id}`, {
+					headers: {
+						"Content-Type": "application/json",
+						"Authorization": `Bearer ${store.token}`
 					}
-					const data = await resp.json();
-					console.log(data)
-					setStore({ company: data });
-					return data; 
-				} catch (error) {
-					console.error('Error fetching company:', error);
-					throw error;  
+				});
+				if (!resp.ok) {
+					throw new Error('Failed to fetch company');
 				}
-			},
-			
-			
-			updateCompany: async (company_id, companyData) => {
-				const store = getStore();
-				const token = store.token; // Asumiendo que almacenas el token en el store
-			
-				try {
-					const response = await fetch(`${process.env.BACKEND_URL}/api/adminportal/${company_id}`, {
-						method: 'PUT',
-						headers: {
-							'Content-Type': 'application/json',
-							'Authorization': `Bearer ${token}`
-						},
-						body: JSON.stringify(companyData)
-					});
-			
-					if (!response.ok) {
-						throw new Error('Failed to update company');
-					}
-			
-					const data = await response.json();
-					return data;
-				} catch (error) {
-					console.error(error);
-					throw error;
-				}
+				const data = await resp.json();
+				console.log(data)
+				setStore({ company: data });
+				return data;
+			} catch (error) {
+				console.error('Error fetching company:', error);
+				throw error;
 			}
-			
-		}
-	};
+		},
 
+
+		updateCompany: async (company_id, companyData) => {
+			const store = getStore();
+			const token = store.token; // Asumiendo que almacenas el token en el store
+
+			try {
+				const response = await fetch(`${process.env.BACKEND_URL}/api/adminportal/${company_id}`, {
+					method: 'PUT',
+					headers: {
+						'Content-Type': 'application/json',
+						'Authorization': `Bearer ${token}`
+					},
+					body: JSON.stringify(companyData)
+				});
+
+				if (!response.ok) {
+					throw new Error('Failed to update company');
+				}
+
+				const data = await response.json();
+				return data;
+			} catch (error) {
+				console.error(error);
+				throw error;
+			}
+		}
+
+	}
 };
 
 export default getState;
