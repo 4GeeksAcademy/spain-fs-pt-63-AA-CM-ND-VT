89dd8c3208a2ae5bb5ed169436097e027f3e1df9
ndl10 <159472389+ndl10@users.noreply.github.com>
2024-06-24 17:28:08 +0000

    commit

--- src/front/js/component/navbar.js
+++ src/front/js/component/navbar.js
@@ -3,6 +3,7 @@ import { Context } from "../store/appContext";
 import { Link, useNavigate } from "react-router-dom";
 import "../../styles/navbar.css";
 
+
 export const Navbar = () => {
 	const { store, actions } = useContext(Context);
 	const navigate = useNavigate();
@@ -20,15 +21,16 @@ export const Navbar = () => {
 		<nav className="navbar navbar-expand-lg navbar-light shadow-sm">
 			<div className="container">
 				<Link to="/" className="navbar-brand">
-					<span className="h4">Home</span>
+					<img src="/eladri.jpg" alt="Descripción de la imagen" height="100"/>
+					
 				</Link>
 				<Link to="/services" className="nav-link">
 					<span className="h4">Services</span>
 				</Link>
 				<button className="navbar-toggler btn-outline-primary" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-				<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="bi bi-person-fill custom-icon" viewBox="0 0 16 16">
-  <path d="M3 14s-1 0-1-1 1-4 6-4 6 3 6 4-1 1-1 1zm5-6a3 3 0 1 0 0-6 3 3 0 0 0 0 6"/>
-</svg>
+					<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="bi bi-person-fill custom-icon" viewBox="0 0 16 16">
+						<path d="M3 14s-1 0-1-1 1-4 6-4 6 3 6 4-1 1-1 1zm5-6a3 3 0 1 0 0-6 3 3 0 0 0 0 6" />
+					</svg>
 				</button>
 				<div className="collapse navbar-collapse justify-content-end" id="navbarNav">
 					{store.token ? (
